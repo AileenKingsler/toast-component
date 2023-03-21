eda4749d9e563faaa346ad2955d489556887e0a9
Alena Tsaregorodtseva <aileen.kingsler@mail.ru>
2023-03-21 16:45:51 +0700

    add keyboard and screen reader support

--- src/components/ToastProvider/ToastProvider.js
+++ src/components/ToastProvider/ToastProvider.js
@@ -5,6 +5,20 @@ export const ToastContext = React.createContext();
 function ToastProvider({ children }) {
   const [toasts, setToasts] = React.useState([]);
 
+  React.useEffect(() => {
+    const handleKeyDown = (event) => {
+      if (event.code === 'Escape') {
+        setToasts([]);
+      }
+    };
+
+    window.addEventListener('keydown', handleKeyDown);
+
+    return () => {
+      window.removeEventListener('keydown', handleKeyDown);
+    };
+  }, []);
+
   const addToast = (variant, message) => {
     const newToasts = [
       ...toasts,
